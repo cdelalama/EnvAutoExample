64aa94d22130ec58f09351b810840ac5512750f6
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-06-26 14:27:14 +0200

    first version

---
+++ src/extension.ts
@@ -0,0 +1,58 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand("EnvAutoExample.generate", () => {
+			const envFile = vscode.workspace.findFiles("**/.env").then((files) => {
+				if (files.length > 0) {
+					const envFilePath = files[0].fsPath;
+					generateEnvExample(
+						envFilePath,
+						path.join(path.dirname(envFilePath), ".env.example")
+					);
+					vscode.window.showInformationMessage("Generated .env.example");
+				} else {
+					vscode.window.showErrorMessage(".env file not found");
+				}
+			});
+		})
+	);
+	vscode.workspace.onDidSaveTextDocument(async (document) => {
+		if (path.basename(document.fileName) === ".env") {
+			const envFilePath = document.fileName;
+
+			const exampleFilePath = path.join(
+				path.dirname(envFilePath),
+				".env.example"
+			);
+
+			if (fs.existsSync(envFilePath)) {
+				generateEnvExample(envFilePath, exampleFilePath);
+				vscode.window.showInformationMessage("Updated .env.example");
+			}
+		}
+	});
+}
+
+function generateEnvExample(envFilePath: string, outputFilePath: string): void {
+	if (!fs.existsSync(envFilePath)) {
+		throw new Error(`File not found: ${envFilePath}`);
+	}
+
+	const envData = fs.readFileSync(envFilePath, { encoding: "utf-8" });
+	const lines = envData.split("\n");
+
+	let outputData = "";
+	for (const line of lines) {
+		if (line.includes("=")) {
+			const [key] = line.trim().split("=", 1);
+			outputData += `${key}=your-${key.toLowerCase()}\n`;
+		} else {
+			outputData += line + "\n";
+		}
+	}
+
+	fs.writeFileSync(outputFilePath, outputData);
+}
