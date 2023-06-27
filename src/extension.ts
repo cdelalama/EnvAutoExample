da23eaaa9476fd1e79c2662507e21fcfb849d0be
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-06-27 15:13:23 +0200

    Improve .env.example auto-generation and error handling
    This commit enhances the functionality and robustness of the .env.example auto-generation feature in the following ways:
    
    Now, the auto-generation function preserves the quotation marks around the values in the .env.example file, if they were present in the original .env file.
    We have improved the TypeScript typing in the extension code, specifically by checking for undefined when splitting environment variable lines and by adding type checks in error handling.
    We have added try-catch blocks around the key processes for better error handling. This will help to avoid unhandled exceptions and will ensure that useful error messages are displayed to the user in case of any failures.

--- src/extension.ts
+++ src/extension.ts
@@ -3,72 +3,93 @@ import * as fs from "fs";
 import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.commands.registerCommand("EnvAutoExample.generate", () => {
-			const envFile = vscode.workspace.findFiles("**/.env").then((files) => {
-				if (files.length > 0) {
-					const envFilePath = files[0].fsPath;
-					generateEnvExample(
-						envFilePath,
-						path.join(path.dirname(envFilePath), ".env.example")
-					);
-					vscode.window.showInformationMessage("Generated .env.example");
-				} else {
-					vscode.window.showErrorMessage(".env file not found");
+	try {
+		context.subscriptions.push(
+			vscode.commands.registerCommand("EnvAutoExample.generate", () => {
+				const envFile = vscode.workspace.findFiles("**/.env").then((files) => {
+					if (files.length > 0) {
+						const envFilePath = files[0].fsPath;
+						generateEnvExample(
+							envFilePath,
+							path.join(path.dirname(envFilePath), ".env.example")
+						);
+						vscode.window.showInformationMessage("Generated .env.example");
+					} else {
+						vscode.window.showErrorMessage(".env file not found");
+					}
+				});
+			})
+		);
+		vscode.workspace.onDidSaveTextDocument(async (document) => {
+			if (path.basename(document.fileName) === ".env") {
+				const envFilePath = document.fileName;
+
+				const exampleFilePath = path.join(
+					path.dirname(envFilePath),
+					".env.example"
+				);
+
+				if (fs.existsSync(envFilePath)) {
+					generateEnvExample(envFilePath, exampleFilePath);
+					vscode.window.showInformationMessage("Updated .env.example");
 				}
-			});
-		})
-	);
-	vscode.workspace.onDidSaveTextDocument(async (document) => {
-		if (path.basename(document.fileName) === ".env") {
-			const envFilePath = document.fileName;
-
-			const exampleFilePath = path.join(
-				path.dirname(envFilePath),
-				".env.example"
-			);
-
-			if (fs.existsSync(envFilePath)) {
-				generateEnvExample(envFilePath, exampleFilePath);
-				vscode.window.showInformationMessage("Updated .env.example");
-			}
-		}
-	});
-	if (vscode.workspace.workspaceFolders) {
-		const watcher = vscode.workspace.createFileSystemWatcher("**/.env");
-		watcher.onDidChange(async (uri) => {
-			const envFilePath = uri.fsPath;
-			const exampleFilePath = path.join(
-				path.dirname(envFilePath),
-				".env.example"
-			);
-
-			if (fs.existsSync(envFilePath)) {
-				generateEnvExample(envFilePath, exampleFilePath);
-				vscode.window.showInformationMessage("Updated .env.example");
 			}
 		});
-		context.subscriptions.push(watcher);
-	}
-}
+		if (vscode.workspace.workspaceFolders) {
+			const watcher = vscode.workspace.createFileSystemWatcher("**/.env");
+			watcher.onDidChange(async (uri) => {
+				const envFilePath = uri.fsPath;
+				const exampleFilePath = path.join(
+					path.dirname(envFilePath),
+					".env.example"
+				);
 
-function generateEnvExample(envFilePath: string, outputFilePath: string): void {
-	if (!fs.existsSync(envFilePath)) {
-		throw new Error(`File not found: ${envFilePath}`);
-	}
-
-	const envData = fs.readFileSync(envFilePath, { encoding: "utf-8" });
-	const lines = envData.split("\n");
-
-	let outputData = "";
-	for (const line of lines) {
-		if (line.includes("=")) {
-			const [key] = line.trim().split("=", 1);
-			outputData += `${key}=your-${key.toLowerCase()}\n`;
+				if (fs.existsSync(envFilePath)) {
+					generateEnvExample(envFilePath, exampleFilePath);
+					vscode.window.showInformationMessage("Updated .env.example");
+				}
+			});
+			context.subscriptions.push(watcher);
+		}
+	} catch (err) {
+		if (err instanceof Error) {
+			vscode.window.showErrorMessage(`Error: ${err.message}`);
 		} else {
-			outputData += line + "\n";
+			vscode.window.showErrorMessage(`An unknown error occurred`);
 		}
 	}
 
-	fs.writeFileSync(outputFilePath, outputData);
+	function generateEnvExample(envFilePath: string, outputFilePath: string): void {
+		try {
+			if (!fs.existsSync(envFilePath)) {
+				throw new Error(`File not found: ${envFilePath}`);
+			}
+
+			const envData = fs.readFileSync(envFilePath, { encoding: "utf-8" });
+			const lines = envData.split("\n");
+
+			let outputData = "";
+			for (const line of lines) {
+				const [key, value] = line.trim().split("=", 2);
+				if (value !== undefined) {
+					let newValue = `your-${key.toLowerCase()}`;
+					// Add quotes if original value had quotes.
+					if (value.startsWith("\"") && value.endsWith("\"")) {
+						newValue = `"${newValue}"`;
+					}
+					outputData += `${key}=${newValue}\n`;
+				} else {
+					outputData += line + "\n";
+				}
+			}
+
+			fs.writeFileSync(outputFilePath, outputData);
+		} catch (err) {
+			if (err instanceof Error) {
+				console.error(`Failed to generate .env.example: ${err.message}`);
+			} else {
+				console.error(`An unknown error occurred`);
+			}
+		}
+	}
 }
